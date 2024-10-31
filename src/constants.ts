5100788041c93dae45d606f92dbe25e7e79c474c
CodePanter <codepanter@gmail.com>
2024-10-31 22:30:59 +0100

    Replaced all rkeys and added one for the delete post.

--- src/constants.ts
+++ src/constants.ts
@@ -1,52 +1,52 @@
 import { Label } from './types.js';
 
-export const DELETE = 'insert-rkey-of-delete-post-here';
+export const DELETE = '3l7tm25wzul2f';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: '3l7tknly4bw2o',
+    rkey: '3l7tm24pwg22u',
     identifier: 'osrslabeler-official-regular-ironman',
     locales: [
       { lang: 'en', name: 'Ironman', description: 'Ironman Account'},
     ]
   },
   {
-    rkey: '3l7tknm5pyz2q',
+    rkey: '3l7tm24vl3c2u',
     identifier: 'osrslabeler-official-hardcore-ironman',
     locales: [
       { lang: 'en', name: 'Hardcore Ironman', description: 'Hardcore Ironman account'},
     ]
   },
   {
-    rkey: '3l7tknmdihm2z',
+    rkey: '3l7tm2533fl2o',
     identifier: 'osrslabeler-official-ultimate-ironman',
     locales: [
       { lang: 'en', name: 'Ultimate Ironman', description: 'Ultimate Ironman account'},
     ]
   },
   {
-    rkey: '3l7tknmj2ce2m',
+    rkey: '3l7tm25aqvd2v',
     identifier: 'osrslabeler-official-maxed',
     locales: [
       { lang: 'en', name: 'Maxed', description: 'Maxed account'},
     ]
   },
   {
-    rkey: '3l7tknmp42v22',
+    rkey: '3l7tm25gaxf22',
     identifier: 'osrslabeler-snowflake-bronzeman',
     locales: [
       { lang: 'en', name: 'Bronzeman', description: 'Bronzeman Account'},
     ]
   },
   {
-    rkey: '3l7tknmufu72p',
+    rkey: '3l7tm25lsfj2q',
     identifier: 'osrslabeler-snowflake-onechunkman',
     locales: [
       { lang: 'en', name: 'One Chunkman', description: 'One Chunkman Account'},
     ]
   },
   {
-    rkey: '3l7tknn22bd2v',
+    rkey: '3l7tm25rcch2r',
     identifier: 'osrslabeler-snowflake-extreme-onechunkman',
     locales: [
       { lang: 'en', name: 'Extreme One Chunkman', description: 'Extreme One Chunkman Account'},
