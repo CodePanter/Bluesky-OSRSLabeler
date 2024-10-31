9dde6a26988cedfde1ddc715e36f7d31b0f4981f
CodePanter <codepanter@gmail.com>
2024-10-31 22:05:25 +0100

    Added all the label rkeys.

--- src/constants.ts
+++ src/constants.ts
@@ -4,49 +4,49 @@ export const DELETE = 'insert-rkey-of-delete-post-here';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7tknly4bw2o',
     identifier: 'osrslabeler-official-regular-ironman',
     locales: [
       { lang: 'en', name: 'Ironman', description: 'Ironman Account'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7tknm5pyz2q',
     identifier: 'osrslabeler-official-hardcore-ironman',
     locales: [
       { lang: 'en', name: 'Hardcore Ironman', description: 'Hardcore Ironman account'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7tknmdihm2z',
     identifier: 'osrslabeler-official-ultimate-ironman',
     locales: [
       { lang: 'en', name: 'Ultimate Ironman', description: 'Ultimate Ironman account'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7tknmj2ce2m',
     identifier: 'osrslabeler-official-maxed',
     locales: [
       { lang: 'en', name: 'Maxed', description: 'Maxed account'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7tknmp42v22',
     identifier: 'osrslabeler-snowflake-bronzeman',
     locales: [
       { lang: 'en', name: 'Bronzeman', description: 'Bronzeman Account'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7tknmufu72p',
     identifier: 'osrslabeler-snowflake-onechunkman',
     locales: [
       { lang: 'en', name: 'One Chunkman', description: 'One Chunkman Account'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7tknn22bd2v',
     identifier: 'osrslabeler-snowflake-extreme-onechunkman',
     locales: [
       { lang: 'en', name: 'Extreme One Chunkman', description: 'Extreme One Chunkman Account'},
